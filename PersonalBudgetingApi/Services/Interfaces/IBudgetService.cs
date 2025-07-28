76c79949548a84a2aaf54b1530c197f8c6622a0b
JavohirAliyev <jakhaaliyev@gmail.com>
2025-07-28 10:03:36 +0500

    global exception handling with custom exceptions

--- PersonalBudgetingApi/Services/Interfaces/IBudgetService.cs
+++ PersonalBudgetingApi/Services/Interfaces/IBudgetService.cs
@@ -6,7 +6,7 @@ namespace PersonalBudgetingApi.Services.Interfaces;
 public interface IBudgetService
 {
     Task<Budget> CreateBudgetAsync(BudgetDto dto, int userId);
-    Task<List<BudgetDto>> GetBudgetsAsync(int userId);
+    Task<List<BudgetDto>> GetBudgetsAsync(int userId, int pageSize, int pageNumber);
     Task<BudgetDto?> GetBudgetByIdAsync(int id, int userId);
     Task<bool> UpdateBudgetAsync(int id, BudgetDto dto, int userId);
     Task<bool> DeleteBudgetAsync(int id, int userId);
